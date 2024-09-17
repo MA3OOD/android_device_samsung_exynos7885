15959dac7365db56bd71ac31ad9638810bf75d9e
Muhammad Masood <163732055+Masood-J@users.noreply.github.com>
2024-09-17 18:46:07 +0500

    Update full_a10.mk

--- a10/full_a10.mk
+++ a10/full_a10.mk
@@ -10,6 +10,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit device configuration
 $(call inherit-product, device/samsung/a10/device.mk)
 
+# Inherit some common rom stuff
+$(call inherit-product, vendor/miku/build/product/miku_product.mk)
+
+BUILD_BROKEN_MISSING_REQUIRED_MODULES := true
+
+# Rom Specific Flags
+MIKU_GAPPS := false
+MIKU_MASTER := Masood
+
+
 BUILD_BROKEN_MISSING_REQUIRED_MODULES := true
 
 # Device identifier. This must come after all inclusions
