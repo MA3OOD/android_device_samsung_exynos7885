7328c448a9e4d8d740aaf37651af6d4fad29c9f0
Muhammad Masood <163732055+Masood-J@users.noreply.github.com>
2024-09-23 19:44:43 +0500

    Update device.mk

--- a10/device.mk
+++ a10/device.mk
@@ -5,5 +5,7 @@ $(call inherit-product, vendor/samsung/a10/a10-vendor.mk)
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-service_32
+# Platform
+PRODUCT_BOARD_PLATFORM := exynos7884
 
 include device/samsung/a10/common.mk
