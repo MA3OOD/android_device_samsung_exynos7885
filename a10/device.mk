14a5c8ad3222da5f5b3974c6cc6aa6ca742bab98
Muhammad Masood <163732055+Masood-J@users.noreply.github.com>
2024-10-05 16:14:31 +0500

    Update device.mk

--- a10/device.mk
+++ a10/device.mk
@@ -1,7 +1,12 @@
 TARGET_LOCAL_ARCH := arm64
 # Platform
-TARGET_BOARD_PLATFORM := exynos7884b
-TARGET_SOC := exynos7884b
+TARGET_BOARD_PLATFORM := $(subst exynos,universal,$(TARGET_SOC))
+TARGET_BOOTLOADER_BOARD_NAME := $(TARGET_SOC)
+
+include hardware/samsung_slsi-linaro/config/BoardConfig7885.mk
+
+TARGET_NO_BOOTLOADER := true
+TARGET_NO_RADIOIMAGE := true
 
 $(call inherit-product, vendor/samsung/a10/a10-vendor.mk)
 
