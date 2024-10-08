aaddd2dae483cc46a453e7d211e94398b42b6930
Muhammad Masood <163732055+Masood-J@users.noreply.github.com>
2024-10-08 22:02:39 +0500

    Update full_a10.mk

--- a10/full_a10.mk
+++ a10/full_a10.mk
@@ -10,8 +10,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit device configuration
 $(call inherit-product, device/samsung/a10/device.mk)
 
-# Inherit some common rom stuff
-$(call inherit-product, vendor/miku/build/product/miku_product.mk)
+# Inherit common Aosp configurations
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := a10
